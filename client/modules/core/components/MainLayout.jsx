8c2a6ebf0828e4fb42376ce4045a9e7dd6358de5
hao <haohuynh2419@gmail.com>
2017-11-09 10:40:46 +0700

    Add some missing packages and Nav bar for website

--- client/modules/core/components/MainLayout.jsx
+++ client/modules/core/components/MainLayout.jsx
@@ -1,12 +1,27 @@
 import React from 'react';
+import {Button, Row, Col, Navbar, NavItem, Nav, NavDropdown, MenuItem, FormGroup, FormControl, Glyphicon, PageFooter, ResponsiveEmbed, Grid} from 'react-bootstrap';
 const Layout = ({content}) => (
-<div>
-<header>
-<h1>Impossible List </h1>
-</header>
-<div>
-{content()}
-</div>
-</div>
+  <Navbar inverse>
+  <Nav>
+    <Navbar.Form pullRight>
+      <FormGroup>
+        <FormControl type="text"  placeholder="Search, grounds,teams or someone..." />
+      </FormGroup>
+      {' '}
+      <Button type="submit" ><Glyphicon glyph= "search"></Glyphicon></Button>
+      </Navbar.Form>
+      <NavItem eventKey={1} href="#">Find Match</NavItem>
+      <NavItem eventKey={2} href="#">Grounds</NavItem>
+      <NavItem eventKey={3} href="#">Messages</NavItem>
+      <NavItem eventKey={4} href="#">Upcoming</NavItem>
+      <NavDropdown  title={
+        <Col xs={6} md={4}>
+          <Glyphicon glyph="user"></Glyphicon></Col>} id="basic-nav-dropdown" >
+      <MenuItem eventKey={1} href="#"><Glyphicon glyph= "log-in"></Glyphicon> Login</MenuItem>
+      <MenuItem eventKey={1} href="#"><Glyphicon glyph= "log-out"></Glyphicon> Logout </MenuItem>
+      <MenuItem eventKey={1} href="#"><Glyphicon glyph= "setting"></Glyphicon> Setting </MenuItem>
+      </NavDropdown>
+  </Nav>
+</Navbar>
 );
 export default Layout
