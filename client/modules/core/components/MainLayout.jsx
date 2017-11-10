f38c00aab0a4e298b36363922ac41cf259c41c60
Thieu <tqthieu96@gmail.com>
2017-11-10 09:46:20 +0700

    fixed logo position

--- client/modules/core/components/MainLayout.jsx
+++ client/modules/core/components/MainLayout.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import {Button, Row, Col, Navbar, NavItem, Nav, NavDropdown, MenuItem, FormGroup, FormControl, Glyphicon, PageFooter, ResponsiveEmbed, Grid} from 'react-bootstrap';
+
 const Layout = ({content}) => (
   <Navbar inverse>
   <Nav>
-  <img width="140px" height="45px" src="https://freetuts.net/upload/tut_post/images/2014/08/14/122/xay-dung-chuc-nang-scrollto-voi-jquery.gif"/>
+  <NavItem eventKey={1} href="#"><img src= "https://www.google.com.vn/url?sa=i&rct=j&q=&esrc=s&source=images&cd=&cad=rja&uact=8&ved=0ahUKEwiW-4_W97LXAhVMu48KHeSbClMQjRwIBw&url=http%3A%2F%2Fwww.vectortemplates.com%2Fraster-batman.php&psig=AOvVaw1QqaZWjsoiICnp7_dMX2zg&ust=1510366452366159" width= "50px" height= "20px"/></NavItem>
   </Nav>
   <Nav>
     <Navbar.Form pullLeft>
@@ -13,13 +14,12 @@ const Layout = ({content}) => (
       {' '}
       <Button type="submit" ><Glyphicon glyph= "search"></Glyphicon></Button>
       </Navbar.Form>
-
       <NavItem eventKey={1} href="#">Find Match</NavItem>
       <NavItem eventKey={2} href="#">Grounds</NavItem>
       <NavItem eventKey={3} href="#">Messages</NavItem>
       <NavItem eventKey={4} href="#">Upcoming</NavItem>
-
   </Nav>
+
   <Nav pullRight>
   <NavDropdown  title={
     <Col xs={6} md={4}>
